9149644c99f0bb835e28706d2f117a89bf3e6eb0
Dino <kill_death@163.com>
2018-04-24 03:05:33 +0800

    MultithreadDemo初步搭建

---
+++ MultithreadDemo/MultithreadDemo/Controller/DORootVC.m
@@ -0,0 +1,25 @@
+//
+//  DORootVC.m
+//  MultithreadDemo
+//
+//  Created by 魏欣宇 on 2018/4/24.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+#pragma mark - Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"多线程学习";
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
