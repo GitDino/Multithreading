e8bc539f414343ebd73f0d828c7d393bdfa86ff4
Dino <kill_death@163.com>
2018-04-24 02:55:25 +0800

    RunLoop-Simple完成

--- RunLoop-Simple/RunLoop-Simple/main.m
+++ RunLoop-Simple/RunLoop-Simple/main.m
@@ -8,10 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+void clickFunction(int type) {
+    printf("正在执行第 %d 条指令", type);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        while (YES) {
+            printf("请输入您的选项：(0表示退出)\n");
+            int result = -1;
+            scanf("%d", &result);
+            if (result == 0)
+            {
+                printf("程序结束！\n");
+                break;
+            }
+            else
+            {
+                clickFunction(result);
+            }
+        }
+        
     }
     return 0;
 }
